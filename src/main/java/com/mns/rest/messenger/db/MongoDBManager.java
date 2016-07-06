782791c8271b35b5ec3124a76bff3d456ece2725
mnsekh111 <mnsekh111@gmail.com>
2016-07-06 18:05:01 -0400

    Added mongoDBManager class

---
+++ src/main/java/com/mns/rest/messenger/db/MongoDBManager.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mns.rest.messenger.db;
+
+/**
+ *
+ * @author mns
+ */
+public class MongoDBManager {
+    
+}
