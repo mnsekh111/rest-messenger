130e7e569a50971c9d634cd7fbac2d061f99c055
mnsekh111 <mnsekh111@gmail.com>
2016-07-07 03:29:16 -0400

    Updated cmds

--- mongo-cmds/cmds.js
+++ mongo-cmds/cmds.js
@@ -29,7 +29,7 @@ db.Users.insert([
     }
     
 ]);
-db.Users.find({"name" : "Sekhar"});
+db.Users.find({"name" : "Sekhar3"});
 db.Users.find({"phone" : {$lte:4500000}});
 
 //And conditional
@@ -37,7 +37,41 @@ db.Users.find({"name" : "Sekhar3","phone":232340});
 
 //Or conditional
 db.Users.find({
-    $or:[{"name" : "Sekhar3","phone":232340}]
+    $or:[{"name" : "Sekhar3"},{"phone":232340}]
 });
+
+//Update only one match
+db.Users.update(
+    {"name":"Sekhar3"},
+    {$set:{"name":"SekharUpdated"}}
+);
+    
+//Update all
+db.Users.update(
+    {"name":"Sekhar3"},
+    {$set:{"name":"SekharUpdated","phone":0000000000}},
+    {multi:true}
+);
+    
+db.Users.save(
+    {"name":"Sekhar3"},
+    {$set:{"name":"SekharUpdated"}},
+    {multi:true}
+);
+
+db.Users.updateMany(
+    {"name":"SekharUpdated"},
+    {$set:{"name":"Sekhar3"}}
+);
+
+
+//Delete an object id
+db.Users.deleteOne(
+    {
+        "_id":ObjectId("577df7e0aa5fce11ac8e987a")
+    }
+)
+
+
 //Find all
 db.Users.find();
