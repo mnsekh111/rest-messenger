173d7d617c760e05f26b1c69da818338b089e3b2
mnsekh111 <mnsekh111@gmail.com>
2016-07-07 03:30:08 -0400

    Initialized mongo connector

--- src/main/java/com/mns/rest/messenger/db/MongoDBManager.java
+++ src/main/java/com/mns/rest/messenger/db/MongoDBManager.java
@@ -5,10 +5,32 @@
  */
 package com.mns.rest.messenger.db;
 
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+
 /**
  *
  * @author mns
  */
 public class MongoDBManager {
+
+    public static void init() {
+        try {
+
+            // To connect to mongodb server
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+            // Now connect to your databases
+            DB db = mongoClient.getDB("test");
+            System.out.println(db.getCollectionNames().toString());
+            System.out.println("Connect to database successfully");
+            //boolean auth = db.authenticate(myUserName, myPassword);
+            //System.out.println("Authentication: " + auth);
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
     
 }
+
