e5a3428b468988457426351a6497dfb3f5d3bb44
mnsekh111 <mnsekh111@gmail.com>
2016-07-09 15:03:51 -0400

    group stub

---
+++ src/main/java/com/mns/rest/messenger/db/DataStore.java
@@ -0,0 +1,7 @@
+package com.mns.rest.messenger.db;
+
+/**
+ * Created by mns on 8/7/16.
+ */
+public class DataStore {
+}
---
+++ src/main/java/com/mns/rest/messenger/model/Message.java
@@ -0,0 +1,7 @@
+package com.mns.rest.messenger.model;
+
+/**
+ * Created by mns on 8/7/16.
+ */
+public class Message {
+}
