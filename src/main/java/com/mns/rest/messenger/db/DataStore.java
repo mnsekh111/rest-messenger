0e69340c250a6e3d559f2416c67788f4d8e9ae1e
mnsekh111 <mnsekh111@gmail.com>
2016-07-10 15:04:43 -0400

    Datastore and message stub

--- src/main/java/com/mns/rest/messenger/db/DataStore.java
+++ src/main/java/com/mns/rest/messenger/db/DataStore.java
@@ -1,7 +1,14 @@
 package com.mns.rest.messenger.db;
+import com.mns.rest.messenger.model.User;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by mns on 8/7/16.
  */
 public class DataStore {
+    private static Map<String,User> userMap = new HashMap<>();
+    public static Map<String,User> getUserMap(){
+        return userMap;
+    }
 }
--- src/main/java/com/mns/rest/messenger/model/Message.java
+++ src/main/java/com/mns/rest/messenger/model/Message.java
@@ -1,7 +1,17 @@
 package com.mns.rest.messenger.model;
 
+import java.util.Date;
+
 /**
  * Created by mns on 8/7/16.
  */
 public class Message {
+    private String id;
+    private String content;
+    private String senderId;
+    private String receiverId;
+    private Date timeStamp;
+
+    public Message() {
+    }
 }
