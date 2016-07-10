4310d01220fe96806ab4957c36a62e1e2480830c
mnsekh111 <mnsekh111@gmail.com>
2016-07-10 01:54:47 -0400

    Message resource + service

---
+++ src/main/java/com/mns/rest/messenger/resources/MessageResource.java
@@ -0,0 +1,7 @@
+package com.mns.rest.messenger.resources;
+
+/**
+ * Created by mns on 8/10/16.
+ */
+public class MessageResource {
+}
---
+++ src/main/java/com/mns/rest/messenger/service/MessageService.java
@@ -0,0 +1,7 @@
+package com.mns.rest.messenger.service;
+
+/**
+ * Created by mns on 8/10/16.
+ */
+public class MessageService {
+}
