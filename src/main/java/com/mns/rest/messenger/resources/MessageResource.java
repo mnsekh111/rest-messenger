85ec51f6ee94b37c826de56c7291f0cd7d6eddb6
mnsekh111 <mnsekh111@gmail.com>
2016-07-10 01:55:07 -0400

    Message resource + service

--- src/main/java/com/mns/rest/messenger/resources/MessageResource.java
+++ src/main/java/com/mns/rest/messenger/resources/MessageResource.java
@@ -1,7 +1,69 @@
 package com.mns.rest.messenger.resources;
 
+import com.mns.rest.messenger.model.Message;
+import com.mns.rest.messenger.service.MessageService;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+
 /**
  * Created by mns on 8/10/16.
  */
+
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class MessageResource {
+    private static MessageService ms = MessageService.getInstance();
+
+
+    @GET
+    public ArrayList<Message> getAllMessages(@PathParam("userId") String userId) {
+        return ms.getMessages(userId);
+    }
+
+    @GET
+    @Path("/{receiverId}")
+    public ArrayList<Message> getAllMessages(@PathParam("receiverId") String receiverId,
+                                             @PathParam("userId") String userId) {
+        return ms.getMessageReceiver(userId, receiverId);
+    }
+
+    @GET
+    @Path("/{receiverId}/{id}")
+    public Message getMessage(@PathParam("receiverId") String receiverId,
+                              @PathParam("userId") String userId,
+                              @PathParam("id") String id) {
+        return ms.getMessage(userId, receiverId, id);
+    }
+
+    @DELETE
+    @Path("/{receiverId}")
+    public void deleteAllMessages(@PathParam("userId") String userId,
+                                  @PathParam("receiverId") String receiverId) {
+        ms.deleteMessageReceiver(userId, receiverId);
+    }
+
+    @DELETE
+    public void deleteAllMessages(@PathParam("userId") String userId) {
+        ms.deleteMessages(userId);
+    }
+
+
+    @DELETE
+    @Path("/{receiverId}/{id}")
+    public void deleteMessage(@PathParam("receiverId") String receiverId,
+                              @PathParam("id") String id,
+                              @PathParam("userId") String userId) {
+        ms.deleteMessage(userId, receiverId, id);
+    }
+
+    @POST
+    @Path("/{receiverId}")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Message addMessage(@PathParam("receiverId") String receiverId,@PathParam("userId") String userId, String message){
+        return ms.addMessage(userId,receiverId,message);
+    }
+
 }
--- src/main/java/com/mns/rest/messenger/service/MessageService.java
+++ src/main/java/com/mns/rest/messenger/service/MessageService.java
@@ -1,7 +1,94 @@
 package com.mns.rest.messenger.service;
 
+import com.mns.rest.messenger.db.DataStore;
+import com.mns.rest.messenger.model.Message;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 /**
  * Created by mns on 8/10/16.
  */
 public class MessageService {
+
+    private Map<String, ArrayList<Message>> userMessageMap = DataStore.getUserMessageMap();
+    private static MessageService messageServiceInstance = null;
+
+    private MessageService() {
+
+    }
+
+    public static MessageService getInstance() {
+        if (messageServiceInstance == null) {
+            messageServiceInstance = new MessageService();
+        }
+        return messageServiceInstance;
+    }
+
+
+    public void initUserMessageStore(String userPhoneNum) {
+        userMessageMap.put(userPhoneNum, new ArrayList<Message>());
+    }
+
+    public Message addMessage(String sender, String receiver, String message) {
+
+        ArrayList<Message> allMessages = new ArrayList<>(userMessageMap.get(sender));
+        ArrayList<Message> allMessagesFromReceiver = getMessageReceiver(sender,receiver);
+        String id = "1";
+        if (allMessagesFromReceiver.size() > 0) {
+            id = "" + (Integer.parseInt(allMessagesFromReceiver.get(allMessagesFromReceiver.size() - 1).getId()) + 1);
+        }
+        Message message1 = new Message(id,message,receiver);
+        allMessages.add(message1);
+        userMessageMap.put(sender,allMessages);
+        return message1;
+    }
+
+    public void deleteMessage(String sender,String receiver,String id){
+        ArrayList<Message> allMessages = new ArrayList<>(userMessageMap.get(sender));
+        allMessages.remove(new Message(id,null,receiver));
+    }
+
+    public void deleteMessages(String sender) {
+        ArrayList<Message> allMessages = new ArrayList<>(userMessageMap.get(sender));
+        allMessages.clear();
+    }
+
+    public void deleteMessageReceiver(String sender,String receiver){
+        ArrayList<Message> filteredMessages = new ArrayList<>();
+        ArrayList<Message> allMessages = new ArrayList<>(userMessageMap.get(sender));
+        for(Message msg : allMessages){
+            if(!msg.getReceiverPhoneNumber().contentEquals(receiver)) {
+                filteredMessages.add(msg);
+            }
+        }
+        allMessages = filteredMessages;
+    }
+
+    public Message getMessage(String sender,String receiver,String id) {
+        ArrayList<Message> allMessagesFromReceiver = getMessageReceiver(sender,receiver);
+        for(Message msg : allMessagesFromReceiver){
+            if(msg.getId().contentEquals(id)){
+                return  msg;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Message> getMessages(String sender){
+        ArrayList<Message> allMessages = new ArrayList<>(userMessageMap.get(sender));
+        return allMessages;
+    }
+
+    public ArrayList<Message> getMessageReceiver(String sender, String receiver) {
+        ArrayList<Message> allMessages = new ArrayList<>(userMessageMap.get(sender));
+        ArrayList<Message> filteredMessages = new ArrayList<>();
+        for (Message msg : allMessages) {
+            if (msg.getReceiverPhoneNumber().contentEquals(receiver)) {
+                filteredMessages.add(msg);
+            }
+        }
+
+        return filteredMessages;
+    }
 }
