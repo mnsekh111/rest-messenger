c82f78b42dae5e383d78f35e89ce17ff677fd0e4
mnsekh111 <mnsekh111@gmail.com>
2016-07-10 01:54:31 -0400

    Data store update

--- src/main/java/com/mns/rest/messenger/db/DataStore.java
+++ src/main/java/com/mns/rest/messenger/db/DataStore.java
@@ -1,5 +1,9 @@
 package com.mns.rest.messenger.db;
+
+import com.mns.rest.messenger.model.Message;
 import com.mns.rest.messenger.model.User;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,8 +11,15 @@ import java.util.Map;
  * Created by mns on 8/7/16.
  */
 public class DataStore {
-    private static Map<String,User> userMap = new HashMap<>();
-    public static Map<String,User> getUserMap(){
+    private static Map<String, User> userMap = new HashMap<>();
+    private static Map<String, ArrayList<Message>> userMessageMap = new HashMap<>();
+
+    public static Map<String, User> getUserMap() {
         return userMap;
     }
+
+    public static Map<String, ArrayList<Message>> getUserMessageMap() {
+        return userMessageMap;
+    }
+
 }
