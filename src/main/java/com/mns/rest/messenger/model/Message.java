a3325523da2074b2c406e7adc6d809fe9b51d74a
mnsekh111 <mnsekh111@gmail.com>
2016-07-11 01:55:22 -0400

    Message (sender removed)

--- src/main/java/com/mns/rest/messenger/model/Message.java
+++ src/main/java/com/mns/rest/messenger/model/Message.java
@@ -1,17 +1,66 @@
 package com.mns.rest.messenger.model;
 
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
  * Created by mns on 8/7/16.
  */
+@XmlRootElement
 public class Message {
     private String id;
     private String content;
-    private String senderId;
-    private String receiverId;
+    private String receiverPhoneNumber;
     private Date timeStamp;
 
     public Message() {
+    }
+
+    public Message(String id, String content, String receiver) {
+        this.id = id;
+        this.content = content;
+        this.receiverPhoneNumber = receiver;
+        this.timeStamp = Calendar.getInstance().getTime();
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+    public String getReceiverPhoneNumber() {
+        return receiverPhoneNumber;
+    }
+
+    public void setReceiverPhoneNumber(String receiverPhoneNumber) {
+        this.receiverPhoneNumber = receiverPhoneNumber;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Message msgObj = (Message) obj;
+        return this.getId().contentEquals(msgObj.getId()) && this.getReceiverPhoneNumber().contentEquals(msgObj.getReceiverPhoneNumber());
     }
 }
